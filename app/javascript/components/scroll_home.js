b6db10daf256f5006a120161e651377f2d36f0aa
george ballard <Theballardian@outlook.com>
2019-03-11 18:51:14 +0800

    added footer, banner and cards to homepage

---
+++ app/javascript/components/scroll_home.js
@@ -0,0 +1,12 @@
+import $ from 'jquery';
+
+const scroll = () => {
+  $(function() {
+    $('a[href="#section06"]').on('click', function(e) {
+      e.preventDefault();
+      console.log('scroll pls')
+      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+    });
+  });
+}
+export { scroll }
