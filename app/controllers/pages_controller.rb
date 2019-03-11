920e9403cab7642af0bc77c24c95344142aadd1e
george ballard <Theballardian@outlook.com>
2019-03-11 16:32:21 +0800

    added routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,25 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only:
+  [:home, :gallery, :tours, :itinerary, :prices, :info, :contact]
 
   def home
+  end
+
+  def gallery
+  end
+
+  def tours
+  end
+
+  def itinerary
+  end
+
+  def prices
+  end
+
+  def info
+  end
+
+  def contact
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/gallery', to: 'pages#gallery'
+  get '/tours', to: 'pages#tours'
+  get '/itinerary', to: 'pages#itinerary'
+  get '/prices', to: 'pages#prices'
+  get '/info', to: 'pages#info'
+  get '/contact', to: 'pages#contact'
 end
