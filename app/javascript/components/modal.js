efb2c8415e53d206c782fd5442a067234ab10a3d
Lorenzo Cursley <lorenzo.xavier87@gmail.com>
2019-03-28 23:43:13 -0400

    fixed issues with navbar, now buttons on burger work and the buttons do no overlap

--- app/javascript/components/modal.js
+++ app/javascript/components/modal.js
@@ -3,6 +3,26 @@ var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById('myImg');
+// var img = document.getElementById('myImg2');
+
+// const ids = ['myImg', 'myImgTwo'];
+
+// var img = document.getElementsByClassName('circle');
+
+
+
+// document.getElementById('click').addEventListener('click', function(){
+//     doStuff();
+//   });
+
+//   function doStuff() {
+//     for(var i=1; i<=; i++){
+//         var el = document.getElementById("myImg" + i);
+//         el.style.display = 'none';
+//     }
+//   }
+
+
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 img.onclick = function(){
